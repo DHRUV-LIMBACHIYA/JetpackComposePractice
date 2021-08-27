a55ffa804f251a657af749dd0ed7a39ee0270d12
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-08-27 19:44:42 +0530

    Basic animations.

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -3,23 +3,65 @@ package com.example.jetpackcomposepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            ComposableWithProduceStateEffect(scaffoldState)
+            BoxAnimation()
         }
     }
 }
 
 
+@Composable
+fun BoxAnimation() {
+    var size by remember {
+        mutableStateOf(200.dp)
+    }
+
+    val sizeState by animateDpAsState(
+        targetValue = size,
+        tween(
+            durationMillis = 1000
+        )
+    )
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Green,
+        targetValue = Color.Red,
+        animationSpec = infiniteRepeatable(
+            tween(durationMillis = 3000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    Box(
+        modifier = Modifier
+            .size(sizeState)
+            .background(color),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { size += 50.dp }) {
+            Text(text = "Increase size")
+        }
+    }
+}
 
 
 
