4d5523656235c6e63a36c5c6427749c08563f006
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-09-06 17:50:28 +0530

    Animated Splash Screen

--- app/src/main/java/com/example/jetpackcomposepractice/composables/navigation/Navigation.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/composables/navigation/Navigation.kt
@@ -1,20 +1,27 @@
 package com.example.jetpackcomposepractice.composables.navigation
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.*
+import com.example.jetpackcomposepractice.R
+import kotlinx.coroutines.delay
 
 /**
  * Created by Dhruv Limbachiya on 06-09-2021.
@@ -25,7 +32,10 @@ fun Navigation() {
     // Get the nav controller.
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+    NavHost(navController = navController, startDestination = Screen.SplashScreen.route) {
+        composable(Screen.SplashScreen.route) {
+            SplashScreen(navController)
+        }
         composable(Screen.MainScreen.route) {
             MainScreen(navController)
         }
@@ -41,6 +51,42 @@ fun Navigation() {
         ) {
             DetailScreen(name = it.arguments?.getString("name"))
         }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        val scale = remember {
+           Animatable(0f)
+        }
+
+        LaunchedEffect(key1 = true) {
+            scale.animateTo(
+                targetValue = 0.5f,
+                animationSpec = tween(
+                    durationMillis = 2000,
+                    easing = {
+                        OvershootInterpolator(2f).getInterpolation(it)
+                    }
+                )
+            )
+
+            delay(3000L)
+
+            navController.navigate(Screen.MainScreen.route)
+        }
+
+        Image(
+            painter = painterResource(id = R.drawable.netflix_splash),
+            contentDescription = "netflix",
+            Modifier.scale(scale.value)
+        )
     }
 }
 
--- app/src/main/java/com/example/jetpackcomposepractice/composables/navigation/Screen.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/composables/navigation/Screen.kt
@@ -5,6 +5,7 @@ package com.example.jetpackcomposepractice.composables.navigation
  */
 
 sealed class Screen(val route: String){
+    object SplashScreen : Screen("splash_screen")
     object MainScreen : Screen("main_screen")
     object DetailScreen : Screen("detail_screen")
 
